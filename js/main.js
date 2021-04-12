bb90eea1a1d274133c39b600cdf9493d20a288a5
Алексей <tayrenov@gmail.com>
2021-04-12 23:14:59 +0300

    L1,L2,L3,L4,L5,L6 Vue.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-new Vue ({
+let L1 = new Vue ({
     el:'#app',
     data: {
         title: "Hello World!",
@@ -11,8 +11,8 @@ new Vue ({
     }
 });
 
-var app4 = new Vue({
-    el: '#app-4',
+let L2 = new Vue({
+    el: '#app-2',
     data: {
         todos: [
           { text: 'Learn JavaScript' },
@@ -20,4 +20,50 @@ var app4 = new Vue({
           { text: 'Build something awesome' }
         ]
       }
+});
+
+let L3 = new Vue({
+    el: '#app-3',
+    data: {
+        value:1
+    },
+
+    methods: {
+        increment(value) {
+            this.value = value;
+            if (value == 25) alert("Число 25");
+        }
+    },
+
+    computed: {
+        doubleValue() {
+            return this.value * 2
+        }
+    }
+
+});
+
+let L4 = new Vue({
+    el: '#app-4',
+    data: {
+        show:false
+    }
+});
+
+let L5_6 = new Vue({
+    el: '#app-5',
+    data: {
+        hello:'HELLO',
+        cars: [
+            {model:"BMW", speed: 250.8},
+            {model:"Audi", speed: 240.21},
+            {model:"Mercedes-Benz AMG", speed: 350.4},
+            {model:"Ford", speed: 160.5}
+        ]
+    },
+    filters: {
+        lovercase (value) {
+            return value.toLowerCase()
+        }
+    }
 });
